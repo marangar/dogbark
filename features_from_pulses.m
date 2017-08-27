22bdca4e416ab972c7dfb2f149e8ed85f16c0915
Marco Angaroni <marcoangaroni@gmail.com>
2017-08-27 17:30:55 +0200

    add spectrum xcross to feature list

--- features_from_pulses.m
+++ features_from_pulses.m
@@ -7,6 +7,8 @@ if nargin < 5
   silent = 0;
 end
 
+pkg load signal;
+
 % ratio between signal-fs and envelope-fs
 r = round(fs / env_fs);
 % truncate W so that is multiple of r
@@ -16,11 +18,13 @@ W = W(:, 1:W_end);
 nt = size(W, 2) / r;
 % frequency-domain features length
 nf = nt;
+% autocorrelation of frequency-domain features length
+nfx = nf;
 
 %
 % output variables
 %
-X = zeros(size(W, 1), nt + nf);
+X = zeros(size(W, 1), nt + nf + nfx);
 
 % calculate fft for every row
 WS = abs(fft(W, [], 2));
@@ -36,20 +40,28 @@ for i = 1:size(W, 1)
   w = W(i, :);
   % ignore negative values
   w(find(w < 0)) = 0;
-  % first half of features is time domain envelope (downsampled of a factor r)
+  % first part of features is time domain envelope (downsampled of a factor r)
   X(i, 1:nt) = fastsmooth(envelope(w, r), 5);
   if (debug)
     figure
-    subplot(2,1,1) 
+    subplot(3,1,1)
     plot(w, 'b', 1:r:length(w), X(i, 1:nt), '-or', 'linewidth', 2)
   end
   % get spectrum of current pulse
   ws = WS(i, :);
-  % second half of features is frequency domain envelope (downsampled of a factor r)
-  X(i, nt+1:end) = envelope(ws, r/2);
+  % second part of features is frequency domain envelope (downsampled of a factor r)
+  wse = envelope(ws, r/2);
+  X(i, nt+1:nt+nf) = wse;
   if (debug)
-    subplot(2,1,2) 
-    plot(ws, 'b', 1:r/2:length(ws), X(i, nt+1:end), '-om', 'linewidth', 2)
+    subplot(3,1,2)
+    plot(ws, 'b', 1:r/2:length(ws), X(i, nt+1:nt+nf), '-om', 'linewidth', 2)
+  end
+  % third part of features is xcorr of frequency domain envelope
+  xwse = xcorr(wse);
+  X(i, nt+nf+1:end) = xwse(nfx:end);
+  if (debug)
+    subplot(3,1,3)
+    plot(X(i, nt+nf+1:end), '-oc', 'linewidth', 2)
   end
 end
 if silent == 0
